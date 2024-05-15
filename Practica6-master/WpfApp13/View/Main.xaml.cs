b862022f3671abfd1183c73f7f2f3d8316b3143b
ksen61 <145546631+ksen61@users.noreply.github.com>
2024-05-15 17:54:49 +0300

    Add files via upload

---
+++ Practica6-master/WpfApp13/View/Main.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using WpfApp13.ViewModel;
+
+namespace WpfApp13.View
+{
+    /// <summary>
+    /// Логика взаимодействия для Main.xaml
+    /// </summary>
+    public partial class Main : Page
+    {
+        public Main()
+        {
+        }
+        public Main(DateTime date)
+        {
+            InitializeComponent();
+            DataContext = new MainVM(date);
+        }
+    }
+}
